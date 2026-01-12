b5a98bd0d40523f1360d267024eaf6ac1c23707c
Michał Kurowski <mkurowski@n7space.com>
2026-01-12 17:48:49 +0100

    Added CLI handling

--- src/TasteDocumentGenerator/Program.cs
+++ src/TasteDocumentGenerator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.IO;
 using CommandLine;
 
 namespace TasteDocumentGenerator;
@@ -13,17 +14,23 @@ class Program
         [Option('t', "template-path", Required = true, HelpText = "Input template file path")]
         public string? TemplatePath { get; set; }
 
-        [Option('i', "interface-view", Required = false, HelpText = "Input Interface View file path")]
-        public string? InterfaceView { get; set; }
+        [Option('i', "interface-view", Required = false, HelpText = "Input Interface View file path", Default = "interfaceview.xml")]
+        public string InterfaceView { get; set; } = "interfaceview.xml";
 
-        [Option('d', "deployment-view", Required = false, HelpText = "Input Deployment View file path")]
-        public string? DeploymentView { get; set; }
+        [Option('d', "deployment-view", Required = false, HelpText = "Input Deployment View file path", Default = "deploymentview.dv.xml")]
+        public string DeploymentView { get; set; } = "deploymentview.dv.xml";
 
         [Option('p', "opus2-model-path", Required = false, HelpText = "Input OPUS2 model file path")]
         public string? Opus2Model { get; set; }
 
         [Option('o', "output-path", Required = true, HelpText = "Output file path")]
         public string? OutputPath { get; set; }
+
+        [Option("target", Required = false, HelpText = "Target system", Default = "ASW")]
+        public string Target { get; set; } = "ASW";
+
+        [Option("template-directory", Required = false, HelpText = "Template directory path", Default = "")]
+        public string TemplateDirectory { get; set; } = "";
     }
 
     [Verb("gui", HelpText = "Launch GUI")]
@@ -47,7 +54,41 @@ class Program
         }).WithParsed<GenerateOptions>(o =>
         {
             Console.WriteLine($"Generating document from {o.TemplatePath}");
+            try
+            {
+                GenerateDocumentCli(o).Wait();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"Error: {e.Message}");
+                Environment.Exit(1);
+            }
         });
+    }
+
+    private static async System.Threading.Tasks.Task GenerateDocumentCli(GenerateOptions options)
+    {
+        var da = new DocumentAssembler();
+
+        var tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(tempDir);
+        try
+        {
+            var context = new DocumentAssembler.Context(
+                options.InterfaceView,
+                options.DeploymentView,
+                options.Target,
+                options.TemplateDirectory,
+                tempDir);
+
+            await da.ProcessTemplate(context, options.TemplatePath!, options.OutputPath!);
+
+            Console.WriteLine($"Document {options.OutputPath} has been successfully created");
+        }
+        finally
+        {
+            Directory.Delete(tempDir, true);
+        }
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
